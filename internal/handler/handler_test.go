4c431e43f0af98154b5d422777cccd038e65c99f
ekovv <denisovdima2004@yandex.ru>
2024-01-23 00:35:36 +0300

    :tada: handler tests ✅

--- internal/handler/handler_test.go
+++ internal/handler/handler_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -80,6 +81,70 @@ func TestHandler_CreateWallet(t *testing.T) {
 
 			if bytes.Equal(wantResponse, response) {
 				t.Errorf("got %s, want %v", w.Body, tt.want)
+			}
+		})
+	}
+}
+
+func TestHandler_Transactions(t *testing.T) {
+	type jso struct {
+		To     string  `json:"to"`
+		Amount float64 `json:"amount"`
+	}
+	tests := []struct {
+		name        string
+		from        string
+		json        jso
+		serviceMock serviceMock
+		wantCode    int
+	}{
+		{
+			name: "OK1",
+			from: "Oz0WKX3YeQ6jRWxJ32Zbxwq17kAdEn",
+			json: jso{
+				To:     "0MEe1ArlKnX5ea0ByX85PD83QBwpJa",
+				Amount: 20.0,
+			},
+			serviceMock: func(c *mocks.Service) {
+				c.Mock.On("Transaction", "Oz0WKX3YeQ6jRWxJ32Zbxwq17kAdEn", "0MEe1ArlKnX5ea0ByX85PD83QBwpJa", 20.0).Return(nil).Times(1)
+			},
+			wantCode: http.StatusOK,
+		},
+		{
+			name: "BAD",
+			from: "Oz0WKX3YeQ6jRWxJ32Zbxwq17kAdEn",
+			json: jso{
+				To:     "jddjkjdksjdkjskd",
+				Amount: 20.0,
+			},
+			serviceMock: func(c *mocks.Service) {
+				c.Mock.On("Transaction", "Oz0WKX3YeQ6jRWxJ32Zbxwq17kAdEn", "jddjkjdksjdkjskd", 20.0).Return(fmt.Errorf("no idOfWallet to person")).Times(1)
+			},
+			wantCode: http.StatusBadRequest,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := gin.Default()
+
+			service := mocks.NewService(t)
+			h := NewHandler(service, config.Config{})
+
+			path := "/api/v1/wallet/:walletId/send"
+			g.POST(path, h.Transactions)
+			tt.serviceMock(service)
+			b, err := json.Marshal(tt.json)
+			if err != nil {
+				return
+			}
+			w := httptest.NewRecorder()
+			request := httptest.NewRequest(http.MethodPost, fmt.Sprintf("/api/v1/wallet/%s/send", tt.from), strings.NewReader(string(b)))
+
+			g.ServeHTTP(w, request)
+
+			if w.Code != tt.wantCode {
+				t.Errorf("got %d, want %d", w.Code, tt.wantCode)
 			}
 		})
 	}
