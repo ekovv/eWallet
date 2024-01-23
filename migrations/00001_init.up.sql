f485ce63063d61676d967f7e5b15ea9cf0d127a2
ekovv <denisovdima2004@yandex.ru>
2024-01-23 16:06:15 +0300

    :tada: migrations ✅

--- migrations/00001_init.up.sql
+++ migrations/00001_init.up.sql
@@ -3,3 +3,10 @@ CREATE TABLE wallets (
      idOfWallet TEXT UNIQUE,
      balance NUMERIC(10, 1)
 );
+
+CREATE TABLE history (
+      time TIMESTAMP WITH TIME ZONE NOT NULL,
+      from_wallet VARCHAR(255) NOT NULL,
+      to_wallet VARCHAR(255) NOT NULL,
+      amount NUMERIC(10, 1) NOT NULL
+);
